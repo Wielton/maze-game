da3696c708d8a4aa974a671b2fe3942d9bce5755
Wielton <grantwielgosz@gmail.com>
2022-05-30 01:05:04 -0600

    player can move around.  Need to implement boundary conditionals

--- app.py
+++ app.py
@@ -11,17 +11,30 @@ print("To move right: d")
 print("Try to get to the end! Good Luck!")
 print("-----------------------------")
 
-# TODO
-# Create a new GameBoard called board
-board = gameboard.GameBoard()
 
-# Create a new Player called played starting at position 3,2
+# A new gameboard has been created
+board = gameboard.GameBoard()
+# A new player was created starting at position 3,2
 player = player.Player(3,2)
 while True:
     board.printBoard(player.rowPosition, player.columnPosition)
     selection = input("Make a move: ")
     # TODO
     # Move the player through the board
+    
+    if (selection == (str.lower("left"))):
+        player.moveLeft()
+    elif (selection == (str.lower("right"))):
+        player.moveRight()
+    elif (selection == (str.lower("up"))):
+        player.moveUp()
+    elif (selection == (str.lower("down"))):
+        player.moveDown()
+    else:
+        print("Please select a direction: left, right, up, down:")
+        input("Make a move: ")
+    
+        
     # Check if the player has won, if so print a message and break the loop!
 
 
