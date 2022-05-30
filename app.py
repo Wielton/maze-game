6a3132a502e70fe4efd168543f33edc1a94277cc
Wielton <grantwielgosz@gmail.com>
2022-05-29 19:31:07 -0600

    The gameboard and player are now revealed

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from ast import Return
 import gameboard
 import player
 
@@ -14,7 +13,7 @@ print("-----------------------------")
 
 # TODO
 # Create a new GameBoard called board
-board = gameboard.GameBoard
+board = gameboard.GameBoard()
 
 # Create a new Player called played starting at position 3,2
 player = player.Player(3,2)
@@ -24,3 +23,17 @@ while True:
     # TODO
     # Move the player through the board
     # Check if the player has won, if so print a message and break the loop!
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------    
+# The overall idea is every "turn" the player takes is one iteration through the infinite loop in the app.py. In this loop you will need to:
+
+# Figure out what direction the user wants to move
+# Check to see if the user can move in that direction
+# Move the user if the move is valid
+# Check to see if the user has won
+
+# Figure out how to change the game board. Make it bigger, add more walls
+# Add "Coins" to collect on the game board. The game should keep track of all the coins collected and tell you your score at the end
+# Add "Enemies" to the game board. If the player moves onto an enemy square they should die and the game should end
+# Add logic to the enemies that allows them to randomly move around the board (also respecting the walls of the game)
